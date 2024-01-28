1d48bcc9eb132aba7bf9692b7c50815a3e4877d1
david.heath <d.p.heath87@gmail.com>
2024-01-28 19:12:58 +0000

    add src and tests folder. move template project into src folder

---
+++ src/Onyx.Products.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Onyx.Products.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HealthCheckController : ControllerBase
+	{
+	}
+}
